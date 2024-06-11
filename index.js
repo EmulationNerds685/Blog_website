1e52c0b056004c3a62c94e3750f3aa03ac7ee073
EmulationNerds685 <bhaskartiwari270@gmail.com>
2024-06-12 01:18:33 +0530

    Features added

--- index.js
+++ index.js
@@ -1,52 +1,86 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-
+import pg from 'pg'
 const app = express()
 const port = 4000
 const posts = []
-
+const db = new pg.Client({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'Task',
+	password: 'Krusty685',
+	port: 5432
+})
+db.connect()
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-	res.render('index.ejs', { posts: posts })
+app.get('/', async (req, res) => {
+	try {
+		const result = await db.query('SELECT * FROM posts')
+		res.render('index.ejs', { posts: result.rows })
+	} catch (err) {
+		console.log('Error displaying')
+	}
 })
 
 app.post('/submit', (req, res) => {
 	res.render('create_post.ejs')
 })
 
-app.post('/create', (req, res) => {
-	const newPost = {
-		title: req.body.title,
-		content: req.body.content,
-		author: req.body.auth
+app.post('/create', async (req, res) => {
+	try {
+		const title = req.body.title
+		const content = req.body.content
+		const author = req.body.auth
+		const result = await db.query(
+			'INSERT INTO posts(title,content,author) VALUES($1,$2,$3)',
+			[title, content, author]
+		)
+		console.log('Post Created!')
+		res.redirect('/')
+	} catch (err) {
+		console.log('Error inserting!')
 	}
-	posts.push(newPost)
-	res.redirect('/')
 })
 
-app.get('/edit/:id', (req, res) => {
-	const postId = req.params.id
-	const post = posts[postId]
-	res.render('edit_post.ejs', { post: post, postId: postId })
-})
+app.post('/edit', async (req, res) => {
+	//	console.log(req.body.ItemId)
+	try {
+		const id = req.body.ItemId
+		const result = await db.query('SELECT * FROM posts WHERE id= $1', [id])
 
-app.post('/update/:id', (req, res) => {
-	const postId = req.params.id
-	const updatedPost = {
-		title: req.body.title,
-		content: req.body.content,
-		author: req.body.auth
+		res.render('edit_post.ejs', { posts: result.rows })
+	} catch (err) {
+		console.log('Error')
 	}
-	posts[postId] = updatedPost
-	res.redirect('/')
 })
 
-app.post('/delete/:id', (req, res) => {
-	const postId = req.params.id
-	posts.splice(postId, 1)
-	res.redirect('/')
+app.post('/update', async (req, res) => {
+	try {
+		const id = req.body.ItemId
+		const title = req.body.title
+		const content = req.body.content
+		const author = req.body.auth
+		const result = await db.query(
+			'UPDATE posts SET title=$1,content=$2,author=$3 WHERE id=$4',
+			[title, content, author, id]
+		)
+		res.redirect('/')
+	} catch (err) {
+		console.log('Error Updating!')
+	}
+})
+
+app.post('/delete', async (req, res) => {
+	try {
+		const id = req.body.deletedItemId
+		const result = db.query('DELETE FROM posts WHERE id=$1', [id])
+		console.log('Deleted!')
+		res.redirect('/')
+	} catch (err) {
+		console.log('Error Deleting!')
+	}
 })
 app.get('/home', (req, res) => {
 	res.redirect('/')
